61f27d8dfb22289b6bc03142a84d5ff213af2b84
CrazyClone55 <shaan@webitup.com>
2021-08-20 15:34:30 -0500

    finish logic/data

--- main.py
+++ main.py
@@ -1,13 +1,70 @@
 import os
+import requests
 import sys
+import time
 import json
+import curses
 import spotipy
 import spotipy.util as util
 import webbrowser
+import argparse
+import urllib.request
 from json.decoder import JSONDecodeError
+import PIL.Image
+import climage
+from pyfiglet import Figlet
+from dotenv import load_dotenv
+
+load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+
 
 #define token
 token = ""
+
+#ascii characters
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+
+#resize image
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return (resized_image)
+    
+#grayscale image
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return (grayscale_image)
+    
+#convert pixels to ascii
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
+    return(characters)
+
+#image converter
+def convertImage(url, new_width=100):
+    urllib.request.urlretrieve(url, "img.png")
+    img = PIL.Image.open("img.png")
+
+    new_image_data = pixels_to_ascii(grayify(resize_image(img)))
+
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+    return(ascii_image)
+
+#color display image
+def displayImage(url):
+    urllib.request.urlretrieve(url, "img.png")
+    img = PIL.Image.open("img.png")
+    output = climage.convert("img.png")
+    return output
+
 
 #login function
 def login(username):
@@ -15,13 +72,13 @@ def login(username):
     scope = 'user-read-private user-read-playback-state user-modify-playback-state'
     try:
         #tries to login
-        token = util.prompt_for_user_token(username, scope)
+        token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
         return token
         print("got token")
     except (AttributeError, JSONDecodeError):
         #if theres an error remove the cache and try again
         os.remove(f".cache-{username}")
-        token = util.prompt_for_user_token(username, scope)
+        token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
         return token
     except (spotipy.SpotifyException):
         #if the environment variables arent set, exit
@@ -69,6 +126,28 @@ else:
 
 spotifyObject=spotipy.Spotify(auth=token)
 
-print("successfully logged in")
+
+
+while True:
+    current = spotifyObject.current_user_playing_track()
+    if current == None:
+        print("nothing is playing")
+        time.sleep(5)
+    else:
+        print(json.dumps(current, sort_keys=True, indent=4))
+        track = current['item']['name']
+        artist = current['item']['artists'][0]['name']
+        imageURL = current['item']['album']['images'][0]['url']
+
+        print(track + " - " + artist)
+        #print(convertImage(imageURL))
+        print(displayImage(imageURL))
+
+        newTrack = current['item']['name']
+
+        while newTrack == track:
+            newTrack = spotifyObject.current_user_playing_track()['item']['name']
+            time.sleep(5)
+
 
 
